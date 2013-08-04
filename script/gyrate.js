4d79c422387ecd532484f36078183fbc9439a255
Puneet Kalra <admin@puneetk.com>
2013-08-04 17:30:28 +0100

    Initial structure ready

---
+++ script/gyrate.js
@@ -0,0 +1,17 @@
+/*
+ * JQGyrate - Alpha v0.9 - 04/08/2013
+ * https://github.com/puneetkay/JQGyrate/
+ *
+ * Copyright (c) 2013 Puneet Kalra
+ * under the MIT license.
+ */
+
+(function($,window,undefined){
+
+	$.fn.gyrate = function(){
+
+		// Home for JQGyrate
+
+	}
+
+})(jQuery,this);
