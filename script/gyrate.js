b7b330dcfb747a622a1ecc32b01f99d8fdf840df
Puneet Kalra <admin@puneetk.com>
2013-08-05 21:20:56 +0100

    Plugin JS base ready, Next is logic.

--- script/gyrate.js
+++ script/gyrate.js
@@ -8,9 +8,46 @@
 
 (function($,window,undefined){
 
-	$.fn.gyrate = function(){
+	$.fn.gyrate = function(options){
 
-		// Home for JQGyrate
+		function initialise(e,o){
+			// Double check options here, or return false.
+			// Once all validated, start gyrator.
+			gyrator(e,o);
+		}
+
+		function gyrator(e,o){
+			
+
+			// Recursive calling.
+			setTimeout(function(){
+				//gyrator(e,o); // Currently disabled for development.
+			},o.speed);
+		}
+
+
+		// Default values
+		var defaults = {
+			speed: 10000, // Speed of gyrator
+			items: {}, // Items for gyrator
+			dance: 'circle', // Dance type, circle, shuffle etc.
+			style: 'fade', // Effects for gyrate, fade, rotateX, rotateY etc.
+			target: 'html' // Target attribute of elements.
+		};
+		
+		// Merge options with defaults.
+		var data = $.extend(defaults, options);
+
+
+		// Data object ready, Now head for logic
+		return this.each(function(){
+			var elem = $(this);
+			if(elem == null)
+				return false;
+
+			initialise(elem, data);
+
+		});
 
 	}
 
